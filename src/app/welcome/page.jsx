d7185bb22bd30e2ff593b309f24369d19e3e87d2
Danil Zharkov <zharkov.danik.ua@gmail.com>
2025-07-18 21:15:19 -0600

    synchronized log in/up button from the navBar with the page

---
+++ src/app/welcome/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function WelcomePage() {
+  return (
+    <div className="w-full min-h-screen flex justify-center">
+      <div className="text-center m-20">
+        <h1 className="text-3xl font-black">welcome to Remember App</h1>
+      </div>
+    </div>
+  )
+}
+
+export default WelcomePage
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,14 +4,18 @@ import { useDispatch } from 'react-redux'
 import Link from 'next/link'
 import { resetAuthForm } from '@/redux/slices/authFormSlice'
 import Button from './Button'
+import { usePathname } from 'next/navigation'
 
 export default function NavBar() {
   const dispatch = useDispatch()
-  const [isClicked, setIsClicked] = useState(false)
-  const handleClick = () => {
-    setIsClicked(!isClicked)
-  }
+  const pathname = usePathname()
 
+  const isSigningUp = pathname === '/register' || pathname === '/welcome'
+  const isSigningIn = pathname === '/auth'
+  let linkHref = ''
+  if (isSigningUp) linkHref = '/auth'
+  if (isSigningIn) linkHref = '/register'
+  
   return (
     <nav className="flex flex-col sm:flex-row items-center justify-center sm:justify-between gap-4 sm:gap-0 px-6 py-4 bg-green-50 drop-shadow-[0_1px_30px_rgba(120,120,80,0.3)]">
       <div className="text-3xl font-bold text-emerald-700 hover:text-emerald-800 transition cursor-default">
@@ -19,13 +23,23 @@ export default function NavBar() {
       </div>
       <div className="flex items-center space-x-4">
         <Link
-          onClick={handleClick}
-          href={isClicked ? '/register' : '/auth'}
-          className={`${isClicked ? 'bg-emerald-600 hover:bg-emerald-700 text-white' : 'bg-stone-200 hover:bg-neutral-300 border-neutral-400 border text-black'} font-semibold px-4 py-2 rounded transition duration-200`}
+          href={linkHref}
+          className={`
+            ${
+              isSigningUp &&
+              'bg-stone-200 hover:bg-neutral-300 border-neutral-400 border text-black'
+            } ${isSigningIn && 'bg-emerald-600 hover:bg-emerald-700 text-white'}
+           font-semibold px-4 py-2 rounded transition duration-200`}
         >
-          {isClicked ? 'Sign up' : 'Sign in'}
+          {isSigningUp && 'Sign in'}
+          {isSigningIn && 'Sign up'}
         </Link>
-        <Button text={'NaN'} className={'w-12 h-12 rounded-full border-[1px] border-lime-500 text-black flex items-center justify-center font-bold hover:scale-105 transition-transform duration-200'}/>
+        <Button
+          text={'NaN'}
+          className={
+            'w-12 h-12 rounded-full border-[1px] border-lime-500 text-black flex items-center justify-center font-bold hover:scale-105 transition-transform duration-200'
+          }
+        />
       </div>
     </nav>
   )
