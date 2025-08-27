a382cb975f6eae8d14914c85d0406b5b871d2e0a
Danil Zharkov <zharkov.danik.ua@gmail.com>
2025-08-27 00:57:07 -0600

    feature/navbar-menu

--- src/components/ProfileMenu.tsx
+++ src/components/ProfileMenu.tsx
@@ -1,21 +1,63 @@
 'use client'
 import { useState, useEffect } from 'react'
-import { User2, Menu, X, UserCircle, MoreHorizontalIcon, MoreVertical, LucideMoreHorizontal, MenuIcon, UserCog, UserRound, UserCircleIcon, LucideUserCircle } from 'lucide-react'
-import Link from 'next/link'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from '@/components/ui/navigation-menu'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+import { Button } from '@/components/ui/button'
 import { useSession, signOut } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import { useDispatch } from 'react-redux'
 import { clearUserInfo } from '@/redux/slices/userInfoSlice'
-
+import {
+  LayoutDashboard,
+  LogOut,
+  LucideDatabase,
+  PersonStanding,
+  Settings,
+  User,
+  UserCheck,
+} from 'lucide-react'
+import Link from 'next/link'
+import P from './P'
 export default function ProfileMenu() {
   const { data: session } = useSession()
   const dispatch = useDispatch()
   const router = useRouter()
+  const [mounted, setMounted] = useState(false)
 
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768)
   const [dropdownOpen, setDropdownOpen] = useState(false)
 
   useEffect(() => {
+    setMounted(true)
     const handleResize = () => setIsMobile(window.innerWidth < 768)
     window.addEventListener('resize', handleResize)
     return () => window.removeEventListener('resize', handleResize)
@@ -30,45 +72,99 @@ export default function ProfileMenu() {
   }
 
   return (
-    <div className="relative z-10">
-      {isMobile && (
-        <button onClick={handleToggle} className='text-green-600 focus:text-green-800 mt-1.5'>
-          <LucideUserCircle size={30} />
-        </button>
-      )}
+    <div className="relative z-50">
+      {mounted && isMobile && (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">
+              <User />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-35" align="start">
+            <DropdownMenuLabel> My Account</DropdownMenuLabel>
+            <DropdownMenuGroup>
+              <Link href={'/profile'}>
+                <DropdownMenuItem>
+                  Profile
+                  <DropdownMenuShortcut>
+                    <User />
+                  </DropdownMenuShortcut>
+                </DropdownMenuItem>
+              </Link>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
 
-      {!isMobile && (
-        <div className="hidden md:flex relative group">
-          <Link
-            href="/profile"
-            className="text-green-600 hover:text-green-800 cursor-pointer transition-colors"
-          >
-            <LucideUserCircle size={30} />
-          </Link>
-          <div className="absolute top-10 -right-2 w-40 p-4 space-y-2 bg-emerald-200 font-black text-black rounded-md shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-600">
-            <Link href="/profile" className="block hover:text-cyan-400">
-              Profile
-            </Link>
-            <button
-              onClick={handleLogOut}
-              className="w-full text-left text-red-400 hover:text-cyan-400"
-            >
+            <DropdownMenuItem onClick={handleLogOut}>
               Log out
-            </button>
-          </div>
-        </div>
+              <DropdownMenuShortcut>
+                <LogOut />
+              </DropdownMenuShortcut>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       )}
-      {isMobile && dropdownOpen && (
-        <div className="fixed top-14 right-2 w-40 p-4 bg-emerald-200 font-black text-black rounded-md shadow-lg space-y-2 z-20 transition-all duration-300">
-          <Link href="/profile" className="block hover:text-cyan-400">
-            Profile
-          </Link>
-          <button
-            onClick={handleLogOut}
-            className="w-full text-left text-red-400 hover:text-cyan-400 z-10"
-          >
-            Log out
-          </button>
+      {mounted && !isMobile && (
+        <div className="flex gap-2">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Other staff</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <NavigationMenuLink href="/">
+                    <div className="flex items-center gap-3">
+                      <P text="Dashboard" classNameText="font-[500]" />
+                      <LayoutDashboard />
+                    </div>
+                  </NavigationMenuLink>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>My account</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <NavigationMenuLink href="/profile">
+                    <div className="flex items-center gap-3">
+                      <P text="Profile" classNameText="font-[500]" />
+                      <User />
+                    </div>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink href="/settings">
+                    <div className="flex items-center gap-3">
+                      <P text="Settings" classNameText="font-[500]" />
+                      <Settings />
+                    </div>
+                  </NavigationMenuLink>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+          <AlertDialog>
+            <AlertDialogTrigger>
+              <Button variant="outline">
+                <LogOut className="text-red-400" />
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. You will be unauthorized.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction
+                  onClick={handleLogOut}
+                  className="bg-emerald-600 hover:bg-emerald-800"
+                >
+                  Continue
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
         </div>
       )}
     </div>
